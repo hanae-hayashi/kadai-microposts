2ce7d6ce4456e1f502e390d1cd9adef2d2c13af0
hanae-hayashi <h-hayashi@sccube.co.jp>
2020-01-12 03:07:57 +0000

    follow, unfollow -fix add some files being ignored before

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row">
-        <aside class="col-sm-4">
+        <aside class="col-sm-2">
             @include('users.card', ['user' => $user])
         </aside>
         <div class="col-sm-8">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="row">
-        <aside class="col-sm-4">
-            @include('users.cards', ['user' => $user])
+        <aside class="col-sm-2">
+            @include('users.card', ['user' => $user])
         </aside>
         <div class="col-sm-8">
             @include('users.navtabs', ['user' =>$user])
